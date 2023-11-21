91249d0612938b15a8d29214c45c1e7218ca9d41
陈阳明 <1450277533@qq.com>
2023-11-21 23:57:26 +0800

    2023_11_21_cym_do_LeetCode_notes

---
+++ leetcode/src/date_11_21/StartMyLeetCode.java
@@ -0,0 +1,9 @@
+package date_11_21;
+
+public class StartMyLeetCode {
+    public static void main(String[] args) {
+        System.out.println("今天看到GitHub上一位同学的库,有感而创,之前都是自己想起做题就做,并没有每天坚持,\n" +
+                "         于是我创建这个项目上传至GitHub上来提醒自己要养成每天做题的好习惯。");
+
+    }
+}
